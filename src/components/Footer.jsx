fc0590139189d2a7e519d2458cf922ea2cc43d37
Harshul Jain <harshulgangwal02@gmail.com>
2022-12-04 22:32:48 +0530

    Added Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer()
 {
     return (
         <div id="footer" style={{textAlign: "center"}}>
-        <h1 style={{textAlign: "center",margin:"100px 0px"}}>FOLLOW ME</h1>
-        <a href="" class="fa fa-github"></a>
-        <a href="" class="fa fa-linkedin"></a>
-        <a href="" class="fa fa-twitter"></a>
+        <h1 style={{textAlign: "center",margin:"5px 0px", fontSize: "20px"}}>Follow Me</h1>
+        <a href="https://github.com/Harshul02" class="fa fa-github"></a>
+        <a href="https://linkedin.com/in/harshul-jain-855027215" class="fa fa-linkedin"></a>
+        <a href="https://twitter.com/Harshuljain02" class="fa fa-twitter"></a>
         <p>Made with  ❤  by Harshul Jain</p>
     </div>
     );
